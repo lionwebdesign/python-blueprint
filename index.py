a90e8650a92a80f2400c4b20efece76fd441e57b
Ruben Villalvazo <vim.ruben@hotmail.com>
2020-12-13 23:19:07 -0300

    Nuevas rutas multiidioma

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from blueprint import blueprint
+from pt_br.pt_bp import pt_bp
+from es_mx.es_bp import es_bp
+from en_us.en_bp import en_bp
 
 
 app = Flask(__name__)
-app.register_blueprint(blueprint)
-
-@blueprint.route('/')
-def index():
-    return "This is an example app"
+app.register_blueprint(pt_bp, url_prefix='/pt-br')
+app.register_blueprint(es_bp, url_prefix='/es-mx')
+app.register_blueprint(en_bp, url_prefix='/en-us')
 
 if __name__ == '__main__':
     app.run(debug=True)
